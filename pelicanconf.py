4e09aaee4cc4697b18751acdea034c58a0bf253e
Tim Staley <tim.staley@soton.ac.uk>
2013-12-04 12:02:16 +0000

    Pelicanconf for peliboots3

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,39 +2,33 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 from collections import OrderedDict
-import os
-from glob import glob
 
 AUTHOR = u'Tim Staley'
 SITENAME = u"Tim Staley"
 SITEURL = 'http://www.timstaley.test'
+
 TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
+RELATIVE_URLS = True
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-DEFAULT_PAGINATION = 3
-DEFAULT_DATE = 'fs'
+THEME='../pelican-bootstrap3'
+BOOTSTRAP_THEME = "flatly"
 
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
-THEME='/home/ts3e11/code/zf-site'
-#THEME='notmyidea'
-#THEME='/home/ts3e11/code/pelican-themes/notmyidea-cms'
-#THEME='/home/ts3e11/code/pelican-themes/tuxlite_zf'
-OUTPUT_PATH='/var/www/'
-
+# 
 ARTICLE_URL='posts/{slug}'
 ARTICLE_SAVE_AS='posts/{slug}/index.html'
 CATEGORY_URL = 'category/{slug}'
 CATEGORY_SAVE_AS = 'category/{slug}/index.html'
 PAGE_URL='{slug}'
 PAGE_SAVE_AS='{slug}/index.html'
-
-STATIC_PATHS= ['images', 'files']
-
+ 
+STATIC_PATHS= ['images', 'files', 'css']
+DEFAULT_DATE = 'fs'
+ 
 # DIRECT_TEMPLATES = ( 'index', 'tags', 'archives', 'articles')
 DIRECT_TEMPLATES = ('blog_index', 'tags', 'categories', 'archives',)
 PAGINATED_DIRECT_TEMPLATES= ('blog_index',)
@@ -55,13 +49,16 @@ TOPBAR_PAGE_LINKS = OrderedDict((
                                  ('BLOG','Blog'),
                                  ))
 
-
 # Blogroll
 # LINKS =  (('Pelican', 'http://getpelican.com/'),
 #           ('Python.org', 'http://python.org/'),
 #           ('Jinja2', 'http://jinja.pocoo.org/'),
 #           ('You can modify those links in your config file', '#'),)
-# Social widget
+# 
+# # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
 #           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
 
